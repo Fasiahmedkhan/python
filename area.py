071937092db363e0f247297caadb86de9cc4a039
Fasi Ahmed Khan J R <148610998+Fasiahmedkhan@users.noreply.github.com>
2024-02-20 15:11:52 +0530

    Update area.py

--- area.py
+++ area.py
@@ -6,7 +6,7 @@ def calculate_area(length, width):
     return area
 
 
-length = float(input("Enter the length of the rectangle: "))
+length = float(input("length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
 
 area = calculate_area(length, width)
