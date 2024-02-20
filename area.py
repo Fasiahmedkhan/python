42bd4ea2a4ae0eab38843a28353d4efa02b86542
Fasi Ahmed Khan J R <148610998+Fasiahmedkhan@users.noreply.github.com>
2024-02-20 14:23:53 +0530

    Update area.py

--- area.py
+++ area.py
@@ -7,7 +7,7 @@ def calculate_area(length, width):
 
 
 length = float(input("Enter the length of the ikm : "))
-width = float(input("Enter the width of the rectangle: "))
+width = float(input("Enter the of the rectangle: "))
 
 area = calculate_area(length, width)
 print("The area of the rectangle is:", area)
