aef346a12518ea4b6323692503ea0eb6a7de66e2
Fasi Ahmed Khan J R <148610998+Fasiahmedkhan@users.noreply.github.com>
2024-02-20 15:05:49 +0530

    Update area.py

--- area.py
+++ area.py
@@ -6,17 +6,10 @@ def calculate_area(length, width):
     return area
 
 
-<<<<<<< HEAD
+
 length = float(input("he dh: "))
-width = float(input("En of the rectangle: "))
-=======
-length = float(input("length of the ikm : "))
-<<<<<<< HEAD
-width = float(input("er the of the rectangle: "))
->>>>>>> 3be40b3983893f51bd46b2823d36272cb168b64c
-=======
-width = float(input("the rectangle: "))
->>>>>>> c65363a65df583bd6d38e93d65a77c38a68cc06c
+width = float(input("Ef the rectangle: "))
+
 
 area = calculate_area(length, width)
 print("The area of the rectangle is:", area)
